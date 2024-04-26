463b0eb43ed349b7c5cecfcb160e3e3c8942064c
Mauro Lucci <maurolucci.14@gmail.com>
2024-04-26 16:28:26 -0300

    Cambios en el orden del notebook

--- instancia.py
+++ instancia.py
@@ -7,21 +7,27 @@ Los vectores se almacenan como cadena de caracteres.
 import networkx as nx
 import ast
 
-def escribir_vector(lista, ruta):
+def escribir_lista(lista, ruta):
     with open(ruta, 'w', encoding="utf-8") as ds:
         ds.write(str(lista))
 
 def escribir_instancia(G, k, u, ruta):
     nx.write_graph6(G, ruta + ".graph")
-    escribir_vector(k, ruta + ".list.k")
-    escribir_vector(u, ruta + ".list.u")
+    escribir_lista(k, ruta + ".list.k")
+    escribir_lista(u, ruta + ".list.u")
 
-def leer_vector(ruta):
+def leer_lista(ruta):
     with open(ruta, 'r', encoding="utf-8") as ds:
         return ast.literal_eval(ds.read())
 
 def leer_instancia(ruta):
     G = nx.read_graph6(ruta + ".graph")
-    k = leer_vector(ruta + ".list.k")
-    u = leer_vector(ruta + ".list.u")
+    k = leer_lista(ruta + ".list.k")
+    u = leer_lista(ruta + ".list.u")
     return G, k, u
+
+def escribir_diccionario(lista, ruta):
+    escribir_lista(lista, ruta)
+
+def leer_diccionario(ruta):
+    return leer_lista(ruta)
